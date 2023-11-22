34eef24f85f268a7c2d684df51c9337dbd443223
SimonPercy <mmmRaliphada809@gmail.com>
2023-11-22 13:00:54 +0200

    connected database

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const dns = require("dns");
-const { MongoClient } = require("mongodb");
-
-const client = new MongoClient(process.env.db);
-
+const mongoose = require("mongoose");
+const dbURI = process.env.DB;
+mongoose
+  .connect(`${dbURI}`, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => console.log("Connected to db"))
+  .catch((err) => console.log(err));
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -20,20 +22,22 @@ app.get("/", function (req, res) {
 app.use(express.urlencoded({ extended: true }));
 
 // Your first API endpoint
-app.post("/api/shorturl", function (req, res) {
+app.post("/api/shorturl", function (req, res, next) {
   const original_url = req.body.url;
 
-  dns.lookup(original_url, (error, address) => {
-    const err = error;
-    //console.log(address, err);
+  dns.lookup(original_url, async (err, address) => {
     if (err) {
       res.json({ error: "invalid url" });
       return;
+    } else {
+      const short_url = Math.floor(Math.random() * 3 + 1);
+      res.json({ original_url, short_url });
     }
   });
-  res.json({ original_url });
 });
-
+app.get(`/api/shorturl/:path`, function (req, res) {
+  res.redirect(original_url);
+});
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });

00ade216755cedf348e621527abae786d8bc111f
SimonPercy <mmmRaliphada809@gmail.com>
2023-11-22 03:24:44 +0200

    install mongodb

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const dns = require("dns");
+const { MongoClient } = require("mongodb");
+
+const client = new MongoClient(process.env.db);
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -18,9 +21,17 @@ app.use(express.urlencoded({ extended: true }));
 
 // Your first API endpoint
 app.post("/api/shorturl", function (req, res) {
-  const url = req.body.url;
+  const original_url = req.body.url;
 
-  res.json({ url });
+  dns.lookup(original_url, (error, address) => {
+    const err = error;
+    //console.log(address, err);
+    if (err) {
+      res.json({ error: "invalid url" });
+      return;
+    }
+  });
+  res.json({ original_url });
 });
 
 app.listen(port, function () {
